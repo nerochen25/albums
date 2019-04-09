40e5de6def3030010ac11866fcf5170dbbba4dc7
Nero <nerochen25@berkeley.edu>
2019-04-08 23:23:59 -0700

    Create src and component folders and header component

--- index.ios.js
+++ index.ios.js
@@ -10,17 +10,14 @@
 
 // Import a library to help create a component
 import React from 'react';
-import ReactNative from 'react-native';
+import { Text, AppRegistry } from 'react-native';
+import Header from './src/components/header';
+
 
 // Create a component
-const App = () => {
-    return (
-        <Text>
-            Some Text
-        </Text>
-    );
-};
-
+const App = () => (
+        <Header />
+);
 
 // Render it to the device
-
+AppRegistry.registerComponent('albums', () => App);
